fc165807fdd8b4b878cfdbc284687fecc7291f87
A Junior <jr@ieee.org>
2020-02-24 01:05:56 -0300

    first commit

---
+++ api/src/main/java/com/abreaspas/quotes/QuotesApplication.java
@@ -0,0 +1,13 @@
+package com.abreaspas.quotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotesApplication.class, args);
+	}
+
+}
